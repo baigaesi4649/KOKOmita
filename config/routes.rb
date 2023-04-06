774a57b1c6dfca898d84100083e80d2c0e6e592d
baigaesi4649 <120004079+baigaesi4649@users.noreply.github.com>
2023-04-06 05:12:35 +0000

    [Add] controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :members
+  # 管理者用
+　# URL /admin/sign_in ...
+　devise_for :admin, skip: [:registrations, :passwords] , controllers: {
+  sessions: "admin/sessions"}
+  # 会員用
+  # URL /members/sign_in ...
+  devise_for :members, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
